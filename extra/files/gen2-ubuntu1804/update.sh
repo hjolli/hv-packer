e50b0c5535b468a3ba6778393691c27d98b94c8c
hjorleifur <hjolli@hjol.li>
2020-03-09 23:00:47 +0100

    fixed docker setup

--- extra/files/gen2-ubuntu1804/update.sh
+++ extra/files/gen2-ubuntu1804/update.sh
@@ -9,12 +9,8 @@ apt-get -y install dkms
 apt-get -y install nfs-common
 apt-get -y install byobu
 apt-get -y ansible apt-transport-https ca-certificates curl software-properties-common python3-pip virtualenv python3-setuptools
+apt-get -y install docker.io
 
-curl -fsSL https://yum.dockerproject.org/gpg | apt-key add - 
-add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main" 
-apt-get update
-apt-get -y install docker-ce docker-compose
+systemctl enable docker
+systemctl start docker
 
